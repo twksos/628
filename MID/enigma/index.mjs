d3491142c04a7bfa9b3ea461afd114c31cc6d0b0
Wei Guangcheng <twksos@qq.com>
2017-10-13 20:53:26 -0400

    finish mid program

--- MID/enigma/index.mjs
+++ MID/enigma/index.mjs
@@ -1,73 +1,121 @@
 import keypress from 'keypress';
-import {Router} from './router';
+import fs from 'fs';
+import {Rotor, Reflector} from './router';
 
-class Reflector {
-    transform(x) {
-        return String.fromCharCode(26 - x.charCodeAt(0));
-    }
-}
 
 class Enigma {
-  constructor() {
-    this.rotors = [new Router(1), new Router(2), new Router(3)];
-    this.reflector = new Reflector();
-    this.plugboard = {};
-    this.mode = 0;
-  }
-  printRouters() {
-    console.log('Router Setting is: ', this.rotors[2].position, this.rotors[1].position, this.rotors[0].position);
-  }
-  setRotor(n) {
-    return (char)=>{
-      this.rotors[n].position = char;
+    constructor() {
+        this.rotors = [new Rotor(1), new Rotor(2), new Rotor(3)];
+        this.reflector = new Reflector();
+        this.plugboard = {};
+        this.mode = 0;
     }
-  }
-  process(key) {
-    const keyAfterPlugboardIn = this.plugboard[key] || key;
-    console.log('keyAfterPlugboardIn', keyAfterPlugboardIn)
-    const keyAfterRoutersIn = this.rotors.reduce((result, rotor)=>{
-      return rotor.transform(result);
-    }, keyAfterPlugboardIn);
-    console.log('keyAfterRoutersIn', keyAfterRoutersIn)
-    const keyReflected = this.reflector.transform(keyAfterRoutersIn);
-    const keyAfterRoutersOut = [this.rotors[2], this.rotors[1], this.rotors[0]].reduce((result, rotor)=>{
-      return rotor.transform(result);
-    }, keyReflected);
-    const result = this.plugboard[keyAfterRoutersOut] || keyAfterRoutersOut;
-    console.log(result);
-    return result;
-  }
+
+    printRouters() {
+        console.log('Router Setting is: ', this.rotors[2].position, this.rotors[1].position, this.rotors[0].position);
+        this.mode = 0;
+    }
+
+    printPlugBoard() {
+        console.log(this.plugboard);
+        this.mode = 0;
+    }
+
+    setRotor(n) {
+        return (char) => {
+            this.rotors[n].position = char;
+        }
+    }
+    setPlugBoard() {
+        fs.readFile('./plugboard.config', 'utf8', (err, config) => {
+            if (err) throw err;
+            console.log(config);
+            config.split(/\n/).forEach((pair)=>{
+                if(!pair) return;
+                if(!pair.toUpperCase().match(/^[A-Z]{2}$/)) console.log('Bad pair:', pair);
+                const char1 = pair[0];
+                const char2 = pair[1];
+                const oldChar1Pair = this.plugboard[char1];
+                if(oldChar1Pair) delete this.plugboard[oldChar1Pair];
+                this.plugboard[char1] = char2;
+
+                const oldChar2Pair = this.plugboard[char2];
+                if(oldChar2Pair) delete this.plugboard[oldChar2Pair];
+                this.plugboard[char2] = char1
+            });
+
+            this.mode = 0;
+        });
+    }
+
+    process(input) {
+        const key = input.toUpperCase();
+        const keyAfterPlugboardIn = this.plugboard[key] || key;
+        const keyAfterRoutersIn = this.rotors.reduce((result, rotor) => {
+            return rotor.transform(result);
+        }, keyAfterPlugboardIn);
+        const keyReflected = this.reflector.transform(keyAfterRoutersIn);
+        const keyAfterRoutersOut = [this.rotors[2], this.rotors[1], this.rotors[0]].reduce((result, rotor) => {
+            return rotor.reverse(result);
+        }, keyReflected);
+        const result = this.plugboard[keyAfterRoutersOut] || keyAfterRoutersOut;
+        console.log(result);
+        return result;
+    }
 }
 
 const enigma = new Enigma();
 // make `process.stdin` begin emitting "keypress" events 
 keypress(process.stdin);
 
+console.log(`
+        0: 'process',
+        1: 'set rotor 1',
+        2: 'set rotor 2',
+        3: 'set rotor 3',
+        4: 'set plugboard',
+        5: 'print rotors'
+`);
 // listen for the "keypress" event 
 process.stdin.on('keypress', function (ch, key) {
-  console.log('ch:', ch);
-  const modeName = {0: 'process', 1: 'set rotor 1', 2: 'set rotor 2', 3: 'set rotor 3', 4: 'set plugboard', 5: 'print rotors'}
-  const action = {
-    0: enigma.process.bind(enigma), 
-    1: enigma.setRotor(0).bind(enigma), 
-    2: enigma.setRotor(1).bind(enigma), 
-    3: enigma.setRotor(2).bind(enigma), 
-    4: ()=>console.log('load plugboard')
-  }
+    if (enigma.mode == 4) return;
+    // console.log('input:', ch.toUpperCase());
+    if (!ch) return;
+    const modeName = {
+        0: 'process',
+        1: 'set rotor 1',
+        2: 'set rotor 2',
+        3: 'set rotor 3',
+        4: 'set plugboard',
+        5: 'print rotors',
+        6: 'print plugboard',
+    };
 
-  if(ch.match(/^[0-5]$/)) {
-    const item = Number(ch);
-    enigma.mode = item;
-    console.log(modeName[enigma.mode]);
-    if(item == 5) {enigma.printRouters(); enigma.mode = 0}
-  } else if(ch.match(/^[a-z]$/)) {
-    action[enigma.mode](ch);
-    enigma.mode = 0;
-  }
-  if (key && key.ctrl && key.name == 'c') {
-    process.stdin.pause();
-  }
+    const action = {
+        0: enigma.process.bind(enigma),
+        1: enigma.setRotor(0).bind(enigma),
+        2: enigma.setRotor(1).bind(enigma),
+        3: enigma.setRotor(2).bind(enigma)
+    };
+    if (ch.match(/^[0-5]$/)) {
+        const item = Number(ch);
+        enigma.mode = item;
+        console.log(modeName[enigma.mode]);
+        if (item == 4) return enigma.setPlugBoard();
+        if (item == 5) enigma.printRouters();
+        if (item == 6) enigma.printPlugBoard();
+        return;
+    }
+
+    if (ch.match(/^[a-z]$/)) {
+        action[enigma.mode](ch);
+        enigma.mode = 0;
+        return;
+    }
+    if (key && key.ctrl && key.name == 'c') {
+        process.stdin.pause();
+    }
 });
- 
+
 process.stdin.setRawMode(true);
 process.stdin.resume();
--- MID/enigma/router.mjs
+++ MID/enigma/router.mjs
@@ -1,17 +1,29 @@
 const CHAR_COUNT = 26;
 const A_CODE = 'A'.charCodeAt(0);
 
-export class Router {
+export class Reflector {
+    transform(x) {
+        const inputCode = x.toUpperCase().charCodeAt(0) - A_CODE;
+        const outputCode = (25 - inputCode) % 26 + A_CODE;
+        // console.log('reflector', inputCode, outputCode);
+        const outputChar = String.fromCharCode(outputCode);
+        return outputChar;
+    }
+}
+
+export class Rotor {
     constructor(advancement) {
-        this.advancement = advancement * 2;
+        this.advancement = advancement;
         this.advancementCounter = 0;
         this._position = 0;
     }
 
     set position(char) {
-        console.log('rotor position set to ', char);
-        return char.toUpperCase().charCodeAt(0) - A_CODE;
+        console.log('rotor position set to ', char.toUpperCase());
+        this._position = char.toUpperCase().charCodeAt(0) - A_CODE;
+        this.advancementCounter = 0;
     }
+
     get position() {
         return String.fromCharCode(this._position + A_CODE);
     }
@@ -19,10 +31,21 @@ export class Router {
     transform(input) {
         const inputCode = input.toUpperCase().charCodeAt(0) - A_CODE;
         const outputCode = (inputCode + this._position) % CHAR_COUNT + A_CODE;
-        this.advancementCounter += 1;
-        if(this.advancementCounter == this.advancement) this._position += 1;
         const resultChar = String.fromCharCode(outputCode);
-        console.log(this.advancement, this._position, input, resultChar);
+        // console.log(this.advancement, this._position, input, resultChar);
+        return resultChar;
+    }
+
+    reverse(input) {
+        const inputCode = input.toUpperCase().charCodeAt(0) - A_CODE;
+        const outputCode = (inputCode + CHAR_COUNT - this._position) % CHAR_COUNT + A_CODE;
+        this.advancementCounter += 1;
+        if (this.advancementCounter == this.advancement) {
+            this.advancementCounter = 0;
+            this._position += 1;
+        }
+        const resultChar = String.fromCharCode(outputCode);
+        // console.log(this.advancement, this._position, input, resultChar);
         return resultChar;
     }
 }
